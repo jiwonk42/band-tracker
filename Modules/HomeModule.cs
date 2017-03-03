e578451fd8361491c29ce8b79c2fced8aa120bd5
Jiwon Kang <jiwonk42@gmail.com>
2017-03-03 14:09:46 -0800

    add index.cshtml, bands_form.cshtml, bands.cshtml, venues_form.cshtml, venues.cshtml

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -2,7 +2,7 @@ using System.Collections.Generic;
 using Nancy;
 using Nancy.ViewEngines.Razor;
 
-namespace RecipeBox
+namespace BandTracker
 {
     public class HomeModule : NancyModule
     {
@@ -14,62 +14,62 @@ namespace RecipeBox
                 List<Venue> AllVenues = Venue.GetAll();
                 return View["venues.cshtml", AllVenues];
             };
-        //     Get["/recipes"] = _ => {
-        //         List<Recipe> AllRecipes = Recipe.GetAll();
-        //         return View["recipes.cshtml", AllRecipes];
-        //     };
+            Get["/bands"] = _ => {
+                List<Band> AllBands = Band.GetAll();
+                return View["bands.cshtml", AllBands];
+            };
+
+            Get["/venues/new"] = _ => {
+                return View["venues_form.cshtml"];
+            };
+            Post["/venues/new"] = _ => {
+                Venue newVenue = new Venue(Request.Form["venue-name"]);
+                newVenue.Save();
+                return View["venues.cshtml", Venue.GetAll()];
+            };
+
+            Get["/bands/new"] = _ => {
+                List<Venue> AllVenues = Venue.GetAll();
+                return View["bands_form.cshtml", AllVenues];
+            };
+            Post["/bands/new"] = _ => {
+                Band newBand = new Band(Request.Form["band-name"], Request.Form["band-ingredient"], Request.Form["band-instruction"], Request.Form["band-rating"]);
+                newBand.Save();
+                return View["bands.cshtml", Band.GetAll()];
+            };
            //
-        //     Get["/venues/new"] = _ => {
-        //         return View["venues_form.cshtml"];
-        //     };
-        //     Post["/venues/new"] = _ => {
-        //         Venue newVenue = new Venue(Request.Form["category-name"]);
-        //         newVenue.Save();
-        //         return View["venues.cshtml", Venue.GetAll()];
-        //     };
-           //
-        //     Get["/recipes/new"] = _ => {
-        //         List<Venue> AllVenues = Venue.GetAll();
-        //         return View["recipes_form.cshtml", AllVenues];
-        //     };
-        //     Post["/recipes/new"] = _ => {
-        //         Recipe newRecipe = new Recipe(Request.Form["recipe-name"], Request.Form["recipe-ingredient"], Request.Form["recipe-instruction"], Request.Form["recipe-rating"]);
-        //         newRecipe.Save();
-        //         return View["recipes.cshtml", Recipe.GetAll()];
-        //     };
-           //
-        //     Get["recipes/{id}"] = parameters => {
+        //     Get["bands/{id}"] = parameters => {
         //         Dictionary<string, object> model = new Dictionary<string, object>();
-        //         Recipe SelectedRecipe = Recipe.Find(parameters.id);
-        //         List<Venue> RecipeVenues = SelectedRecipe.GetVenues();
+        //         Band SelectedBand = Band.Find(parameters.id);
+        //         List<Venue> BandVenues = SelectedBand.GetVenues();
         //         List<Venue> AllVenues = Venue.GetAll();
-        //         model.Add("recipe", SelectedRecipe);
-        //         model.Add("recipeVenues", RecipeVenues);
+        //         model.Add("band", SelectedBand);
+        //         model.Add("bandVenues", BandVenues);
         //         model.Add("allVenues", AllVenues);
-        //         return View["recipe.cshtml", model];
+        //         return View["band.cshtml", model];
         //     };
         //     Get["/venues/{id}"] = parameters => {
         //         Dictionary<string, object> model = new Dictionary<string, object>();
         //         var SelectedVenue = Venue.Find(parameters.id);
-        //         var VenueRecipes = SelectedVenue.GetRecipes();
-        //         List<Recipe> AllRecipes = Recipe.GetAll();
+        //         var VenueBands = SelectedVenue.GetBands();
+        //         List<Band> AllBands = Band.GetAll();
         //         model.Add("category", SelectedVenue);
-        //         model.Add("categoryRecipes", VenueRecipes);
-        //         model.Add("allRecipes", AllRecipes);
+        //         model.Add("categoryBands", VenueBands);
+        //         model.Add("allBands", AllBands);
         //         return View["category.cshtml", model];
         //     };
            //
-        //     Post["/recipe/add_category"] = _ => {
+        //     Post["/band/add_category"] = _ => {
         //         Venue category = Venue.Find(Request.Form["category-id"]);
-        //         Recipe recipe = Recipe.Find(Request.Form["recipe-id"]);
-        //         recipe.AddVenue(category);
-        //         return View["category-list.cshtml", recipe.GetVenues()];
+        //         Band band = Band.Find(Request.Form["band-id"]);
+        //         band.AddVenue(category);
+        //         return View["category-list.cshtml", band.GetVenues()];
         //     };
-        //     Post["/category/add_recipe"] = _ => {
+        //     Post["/category/add_band"] = _ => {
         //         Venue category = Venue.Find(Request.Form["category-id"]);
-        //         Recipe recipe = Recipe.Find(Request.Form["recipe-id"]);
-        //         category.AddRecipe(recipe);
-        //         return View["recipe-list.cshtml", category.GetRecipes()];
+        //         Band band = Band.Find(Request.Form["band-id"]);
+        //         category.AddBand(band);
+        //         return View["band-list.cshtml", category.GetBands()];
         //     };
            //
         //     Patch["/category/edit/{id}"] = parameters => {
@@ -77,16 +77,16 @@ namespace RecipeBox
         //         SelectedVenue.UpdateVenues(Request.Form["new-category-name"]);
         //         return View["venues.cshtml", Venue.GetAll()];
         //     };
-        //     Patch["/recipe/edit/{id}"] = parameters => {
-        //         Recipe SelectedRecipe = Recipe.Find(parameters.id);
-        //         SelectedRecipe.UpdateRecipes(Request.Form["new-recipe-name"], Request.Form["new-recipe-ingredient"], Request.Form["new-recipe-instruction"], Request.Form["new-recipe-rating"]);
-        //         return View["recipes.cshtml", Recipe.GetAll()];
+        //     Patch["/band/edit/{id}"] = parameters => {
+        //         Band SelectedBand = Band.Find(parameters.id);
+        //         SelectedBand.UpdateBands(Request.Form["new-band-name"], Request.Form["new-band-ingredient"], Request.Form["new-band-instruction"], Request.Form["new-band-rating"]);
+        //         return View["bands.cshtml", Band.GetAll()];
         //     };
            //
-        //     Get["/recipes/detail/{id}"] = parameters =>
+        //     Get["/bands/detail/{id}"] = parameters =>
         //     {
-        //         Recipe recipe = Recipe.Find(parameters.id);
-        //         return View["recipe-detail.cshtml", recipe];
+        //         Band band = Band.Find(parameters.id);
+        //         return View["band-detail.cshtml", band];
         //     };
            //
         //     Delete["/venues/{id}"] = parameters =>
@@ -95,27 +95,27 @@ namespace RecipeBox
         //         targetVenue.Delete();
         //         return View["venues.cshtml", Venue.GetAll()];
         //     };
-        //     Delete["/recipes/{id}"] = parameters =>
+        //     Delete["/bands/{id}"] = parameters =>
         //     {
-        //         Recipe targetRecipe = Recipe.Find(parameters.id);
-        //         targetRecipe.Delete();
-        //         return View["recipes.cshtml", Recipe.GetAll()];
+        //         Band targetBand = Band.Find(parameters.id);
+        //         targetBand.Delete();
+        //         return View["bands.cshtml", Band.GetAll()];
         //     };
            //
-        //     Post["/recipe/search/results"] = _ => {
-        //        List<Recipe> FoundList = new List<Recipe>{};
-        //        Recipe foundRecipe = Recipe.FindByIngredient(Request.Form["recipe-search"]);
-        //        FoundList.Add(foundRecipe);
+        //     Post["/band/search/results"] = _ => {
+        //        List<Band> FoundList = new List<Band>{};
+        //        Band foundBand = Band.FindByIngredient(Request.Form["band-search"]);
+        //        FoundList.Add(foundBand);
         //        return View["search.cshtml", FoundList];
         //    };
            //
         //     Post["/venues/delete"] = _ => {
         //         Venue.DeleteAll();
-        //         Recipe.DeleteAll();
+        //         Band.DeleteAll();
         //         return View["index.cshtml"];
         //     };
-        //     Post["/recipes/delete"] = _ => {
-        //         Recipe.DeleteAll();
+        //     Post["/bands/delete"] = _ => {
+        //         Band.DeleteAll();
         //         return View["index.cshtml"];
         //     };
         }
